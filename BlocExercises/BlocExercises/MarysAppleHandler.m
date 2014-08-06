53c03ce426a8d750b064097447b8009fd90d7d26
startupguysandy <andy@thestartupguys.co.uk>
2014-08-06 23:33:26 +0100

    Completed equality

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,6 +15,18 @@
 
     /* WORK HERE */
     
+    if(dollars >= 1000000000) {
+        itemToReturn = @"have The Big Apple";
+    } else if (dollars >= 1000) {
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 6) {
+        itemToReturn = @"have an apple";
+    } else if (dollars >= 5) {
+        itemToReturn = @"have some gum";
+    } else {
+        itemToReturn = @"get out of my store";
+    }
+    
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
