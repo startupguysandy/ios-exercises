484ba3c1203d84eb06db1fcf9c5d823dcf640d43
startupguysandy <andy@thestartupguys.co.uk>
2014-08-09 13:19:26 +0100

    completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,24 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    return characterDictionary[@"favorite drink"];
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *favoriteDrinks = [[NSMutableArray alloc] init];
+    for (NSDictionary *characterDrink in charactersArray) {
+        NSString *favoriteDrink = characterDrink[@"favorite drink"];
+        [favoriteDrinks addObject:favoriteDrink];
+    }
+    return favoriteDrinks;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *trekCharacters = [characterDictionary mutableCopy];
+    [trekCharacters setObject:@"Get going!" forKey:@"quote"];
+    return trekCharacters;
 }
 
 @end
