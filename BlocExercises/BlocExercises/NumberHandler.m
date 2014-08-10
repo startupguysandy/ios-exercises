b696c0b5c7ce955c079dfc7238f69ea99296057b
startupguysandy <andy@thestartupguys.co.uk>
2014-08-10 12:53:30 +0100

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,39 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    //  First convert the numberto an integer so we can work with it
+    NSInteger intConversion = [number integerValue];
+    
+    // Return the result multiplied by 2
+    return [NSNumber numberWithInteger:intConversion*2];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    NSInteger lowNumber = (number <= otherNumber) ? number : otherNumber;
+    NSInteger highNumber = (number <= otherNumber) ? otherNumber : number;
+    
+    while (lowNumber <= highNumber) {
+        [numberArray addObject:[NSNumber numberWithInteger:lowNumber]];
+        lowNumber++;
+    };
+    
+    return numberArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    // Create a sorted version of our numbers passed in
+    NSArray *listInOrder = [arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+    NSLog(@"array: %@", listInOrder);
+    
+    // Grab the last number
+    NSNumber *lowestNumber = listInOrder[0];
+    
+    // Pass in the lowest number as an integer value
+    return lowestNumber.integerValue;
 }
 
 @end
